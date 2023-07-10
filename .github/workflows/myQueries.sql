e810ff02176bee86becc025649b1e1f8abff8def
anitharavuri21 <ravurilavanya21@gmail.com>
2023-07-10 11:01:28 +0000

    added comments

--- .github/workflows/myQueries.sql
+++ .github/workflows/myQueries.sql
@@ -1,9 +1,22 @@
+
+--created indexes to optimize the performance of the queries
+
 create index passenger_GIndx on Airline(Gender);
-select count(*) from Airline where Gender='Female';
 create index passenger_ClassIndx on Airline(Class);
-select count(*) from Airline where Class='Business';
-select count(*) from Airline where Class='Eco';
 create index passenger_AgeIndx on Airline(Age);
-select count(*) from Airline where Age<13;
 create index passenger_GAIndx on Airline(Age,Class);
+
+--(1) Write a SQL Query to find the number of female passengers who have made bookings for air tickets?
+select count(*) from Airline where Gender='Female';
+
+--(2) Write a SQL Query to find the total number of passengers who have booked tickets in the business class?
+select count(*) from Airline where Class='Business';
+
+--(3) Write a SQL Query to find the number of passengers who have booked tickets in the 'eco' class?
+select count(*) from Airline where Class='Eco';
+
+--(4)Write a SQL Query to find the number of children's who are all book the tickets?
+select count(*) from Airline where Age<13;
+
+--(5)Write a SQL Query to count of passengers who are above the age of 16 and have booked tickets in the business class?
 select count(*) from Airline where Age>16 and Class='Business';
