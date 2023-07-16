f89e3158bff166d3a14550e10e17d074ff359953
sree-lekha02 <saisreeyv123@gmail.com>
2023-07-16 13:30:32 +0000

    code

---
+++ .github/workflows/first.sql
@@ -0,0 +1,22 @@
+select * from airline;
+
+select count(*) as female_passengers
+from airline
+where GENDER='Female';
+
+select count(*) as total_passengers
+from airline
+where CLASS='Business';
+
+select count(*) as num_of_passengers
+from airline
+where CLASS='Eco';
+
+select count(*) as count
+from airline
+where AGE<13; 
+
+select count(*) as count_of_passengers
+from airline
+where AGE>16 and CLASS='Business';
+
