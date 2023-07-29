f210670b76f552c11c4089c906f398f74ef93751
Rishitha-Alluri <rishithasrinivas222@gmail.com>
2023-07-29 12:06:15 +0000

    Rectified

--- Rishitha-Alluri.sql
+++ Rishitha-Alluri.sql
@@ -1,9 +1,9 @@
 select count(*) as count from AIRLINE where GENDER='Female';
 
-select count(*) as count from AIRLINE where CLASS like 'Business';
+select count(*) as count from AIRLINE where CLASS = 'Business';
 
-select count(*) as count from AIRLINE where CLASS like 'Eco';
+select count(*) as count from AIRLINE where CLASS = 'Eco';
 
 select count(*) as count from AIRLINE where AGE<13;
 
-select count(*) as count from AIRLINE where AGE>16 and CLASS like 'Business';
+select count(*) as count from AIRLINE where AGE>16 and CLASS = 'Business';
