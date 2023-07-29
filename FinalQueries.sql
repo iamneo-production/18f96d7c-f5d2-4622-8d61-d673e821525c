a9ae456cc94aeef1516cc91bdaedff1f9679f58d
Rishitha-Alluri <rishithasrinivas222@gmail.com>
2023-07-29 09:57:30 +0000

    Deleted.

--- FinalQueries.sql
+++
@@ -1,9 +0,0 @@
-select count(*) as count from AIRLINE where GENDER='Female';
-
-select count(*) as count from AIRLINE where CLASS like 'Business';
-
-select count(*) as count from AIRLINE where CLASS like 'Eco';
-
-select count(*) as count from AIRLINE where AGE<13;
-
-select count(*) as count from AIRLINE where AGE>16 and CLASS like 'Business';
